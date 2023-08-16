e19105633f16c46caed25265f1b87a35e70a4e0a
Chandan Verma <chandan.verma@apcompinfotech.com>
2023-08-16 19:02:46 +0530

    Update:- Setup

--- frontend/config/auth.php
+++ frontend/config/auth.php
@@ -40,13 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-     
-        'api' => [
-            'driver' => 'passport',
-            'provider' => 'users',
-        ],
     ],
-
 
     /*
     |--------------------------------------------------------------------------
